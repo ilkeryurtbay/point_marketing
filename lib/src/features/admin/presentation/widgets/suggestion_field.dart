0a32e6c23a4a4dba7c37d212596a817144b0e9ae
Alper Karakay <alp.karky@gmail.com>
2023-10-08 00:54:54 +0300

    added validation for some of the suggestion fields

--- lib/src/features/admin/presentation/widgets/suggestion_field.dart
+++ lib/src/features/admin/presentation/widgets/suggestion_field.dart
@@ -3,14 +3,19 @@ part of 'package:point_marketing/src/features/admin/presentation/admin_page.dart
 class SuggestionField<T extends ISuggestionModel> extends StatefulWidget {
   final TextEditingController controller;
   final FutureOr<Iterable<T>> Function(String) getSuggestionMethod;
-
   final String labelText;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final void Function(bool)? onSuggestionSelected; // Add this callback
 
   const SuggestionField({
     Key? key,
     required this.controller,
     required this.getSuggestionMethod,
     required this.labelText,
+    this.onChanged,
+    this.validator,
+    this.onSuggestionSelected,
   }) : super(key: key);
 
   @override
@@ -21,12 +26,24 @@ class _SuggestionFieldState<T extends ISuggestionModel>
     extends State<SuggestionField<T>> {
   @override
   Widget build(BuildContext context) {
-    return TypeAheadField<T?>(
+    bool showValidation =
+        Provider.of<ValidationProvider>(context).showValidation;
+    return TypeAheadFormField<T?>(
+      autovalidateMode:
+          showValidation ? AutovalidateMode.always : AutovalidateMode.disabled,
+      validator: (value) {
+        return widget.controller.text.trim().isEmpty
+            ? 'Lütfen bu alanı doldurun'
+            : null;
+      },
       textFieldConfiguration: TextFieldConfiguration(
         controller: widget.controller,
+        onChanged: widget.onChanged,
         decoration: InputDecoration(
-            labelText: widget.labelText,
-            suffixIcon: const Icon(Icons.keyboard_arrow_down_outlined)),
+          labelText: widget.labelText,
+          suffixIcon: const Icon(Icons.keyboard_arrow_down_outlined),
+          //errorText: 'Lütfen bu alanı doldurun',
+        ),
       ),
       suggestionsCallback: widget.getSuggestionMethod,
       itemBuilder: (context, suggestion) {
@@ -42,6 +59,7 @@ class _SuggestionFieldState<T extends ISuggestionModel>
         setState(() {
           widget.controller.text = suggestion?.name ?? '';
         });
+        widget.onSuggestionSelected?.call(suggestion != null);
       },
       noItemsFoundBuilder: (context) => const SizedBox(),
       debounceDuration: const Duration(milliseconds: 800),
